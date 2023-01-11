a1b4b262618b47b6b039cea0b06f4e3313edb368
AdeSetiyawan <53717955+AdeSetiyawan@users.noreply.github.com>
2023-01-11 09:28:09 +0700

    pengambilan nilai blade system

--- XIIRPL2(❁´◡`❁)(❁´◡`❁)(❁´◡`❁)(u‿ฺu✿ฺ)/crudlara_maryoko/routes/web.php
+++ XIIRPL2(❁´◡`❁)(❁´◡`❁)(❁´◡`❁)(u‿ฺu✿ฺ)/crudlara_maryoko/routes/web.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\TambahDataBarangController;
+use App\Http\Controllers\TampilDataBarangController;
+use App\Http\Controllers\HapusDataBarangController;
+use App\Http\Controllers\PerbaharuiDataBarangController;
 // untuk menggunakan controllers harus dipanggil dulu
 
 Route::get('/',[DashboardController::class,'index']); 
-//ini adalah landing page karena diberi jalur dengan nama "/"
+Route::get('/tambah',[TambahDataBarangController::class,'index']); 
+Route::get('/tampil',[TampilDataBarangController::class,'index']); 
+Route::get('/hapus',[HapusDataBarangController::class,'index']); 
+Route::get('/perbaharui',[PerbaharuiDataBarangController::class,'index']); 
